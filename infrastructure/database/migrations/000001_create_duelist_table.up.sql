78ba7fe143fc6125ddbbe604423c6a6c88215d03
JPauloMoura <joaopaulo.034a@gmail.com>
2024-02-08 10:46:10 -0300

    chore(setup): :hammer: update script to create table duelist

--- infrastructure/database/migrations/000001_create_duelist_table.up.sql
+++ infrastructure/database/migrations/000001_create_duelist_table.up.sql
@@ -1,13 +1,13 @@
 CREATE TABLE  IF NOT EXISTS duelists (
     ID VARCHAR(50) PRIMARY KEY,
     Name VARCHAR(255) NOT NULL,
+    Presentation TEXT,
     BirthDate DATE NOT NULL,
-    Street VARCHAR(255) NOT NULL,
-    City VARCHAR(255) NOT NULL,
     State CHAR(2) NOT NULL,
-    PostalCode VARCHAR(10) NOT NULL,
-    Complement VARCHAR(255),
+    City VARCHAR(255) NOT NULL,
+    Street VARCHAR(255) NOT NULL,
+    District VARCHAR(255) NOT NULL,
+    Cep VARCHAR(10) NOT NULL,
     Email VARCHAR(255) NOT NULL,
-    Phone VARCHAR(20) NOT NULL,
-    Presentation TEXT
+    Phone VARCHAR(20) NOT NULL
 );
