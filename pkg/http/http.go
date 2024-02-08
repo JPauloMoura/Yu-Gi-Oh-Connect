70d3a66fa1a026c8fe2618b67303e4965969a5d7
JPauloMoura <joaopaulo.034a@gmail.com>
2024-02-08 01:07:54 -0300

    feat: :sparkles: create wrapper to http client

---
+++ pkg/http/http.go
@@ -0,0 +1,13 @@
+package http
+
+import (
+	"net/http"
+)
+
+type HttpClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+func NewHttpClient() HttpClient {
+	return http.DefaultClient
+}
